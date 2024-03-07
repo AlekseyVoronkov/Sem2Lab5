947adc1a0d2eca52dd818facf0fbed2eae5d82cb
AlekseyVoronkov <144922364+AlekseyVoronkov@users.noreply.github.com>
2024-03-07 19:24:02 +0700

    Create Sem2Lab5.cs

---
+++ Sem2Lab5.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        bool isRunning = true;
+
+        while (isRunning)
+        {
+
+            Dictionary<string, string> errorWords = new Dictionary<string, string>
+        {
+            { "привет", "првиет" },
+            { "пока", "пака"  }
+        };
+
+            Console.WriteLine("Choose your directory: ");
+
+            string directoryPath = Console.ReadLine();
+
+            Console.WriteLine("Type 1 if you want to fix typos in file");
+            Console.WriteLine("Type 2 if you want to replace phone numbers in file");
+            Console.WriteLine("Type 3 to exit");
+
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+            DirectoryInfo directory = new DirectoryInfo(directoryPath);
+            FileInfo[] files = directory.GetFiles("*.txt");
+
+            if (userChoice != 1 && userChoice != 2 && userChoice != 3)
+            {
+                Console.WriteLine("you're not the brigthest one, huh? Idk, try again");
+
+                userChoice = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if (userChoice == 1)
+            {
+                foreach (var file in files)
+                {
+                    string content = File.ReadAllText(file.FullName);
+
+                    foreach (var errorWord in errorWords)
+                    {
+                        content = content.Replace(errorWord.Value, errorWord.Key);
+                    }
+
+                    File.WriteAllText(file.FullName, content);
+                }
+
+                Console.WriteLine("Typos have been fixed :D ");
+            }
+
+            if (userChoice == 2)
+            {
+                foreach (var file in files)
+                {
+                    string content = File.ReadAllText(file.FullName);
+
+                    content = Regex.Replace(content, @"\(\d{3}\) \d{3}-\d{2}-\d{2}", "+380 12 345 67 89");
+
+                    File.WriteAllText(file.FullName, content);
+                }
+
+                Console.WriteLine("Phone numbers have been changed :D ");
+            }
+
+            if (userChoice == 3)
+            {
+                Console.WriteLine("death :D");
+                Environment.Exit(0);
+            }
+        }
+    }
+}
